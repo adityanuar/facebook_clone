33bd5ca4c58739bf4d5c08700e9a62f533bbda0d
Adit <adityanuar@gmail.com>
2022-07-24 11:52:06 +0700

    add login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,13 @@
-const { validateEmail, validateLength } = require("../helpers/validation");
+const {
+  validateEmail,
+  validateLength,
+  validateUsername,
+} = require("../helpers/validation");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const { generateToken } = require("../helpers/tokens");
+const { sendVerificationEmail } = require("../helpers/mailer");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -43,21 +50,85 @@ exports.register = async (req, res) => {
         .json({ message: "Password must be between 3 and 30 characters!" });
     }
     const cryptedPassword = await bcrypt.hash(password, 12);
-    console.log(cryptedPassword);
-    return;
-    const user = new User({
+    let tempUsername = first_name + last_name;
+    let newUsername = await validateUsername(tempUsername);
+    const user = await new User({
       first_name,
       last_name,
       email,
-      password,
-      username,
+      password: cryptedPassword,
+      username: newUsername,
       bYear,
       bMonth,
       bDay,
       gender,
     }).save();
-    res.json(user);
+    const emailVerificationToken = generateToken(
+      { id: user._id.toString() },
+      "30m"
+    );
+    const url = `${process.env.BASE_URL}/activate/${emailVerificationToken}`;
+    sendVerificationEmail(user.email, user.first_name, url);
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Register Success! please activate your email to start",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+  console.log(user);
+  const check = await User.findById(user.id);
+  console.log(check);
+  if (check.verified == true) {
+    return res.status(400).json({ mesage: "this email is already activated!" });
+  } else {
+    await User.findByIdAndUpdate(user.id, { verified: true });
+    return res
+      .status(200)
+      .json({ message: "Account has been activated successfully" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "the email address you entered is not connected to an account",
+      });
+    }
+    const check = await bcrypt.compare(password, user.password);
+    console.log(check);
+    if (!check) {
+      return res.status(400).json({
+        message: "Invalid credential. Please try again.",
+      });
+    }
+    const token = generateToken({ id: user._id.toString() }, "7d");
+    res.send({
+      id: user._id,
+      username: user.username,
+      picture: user.picture,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      token: token,
+      verified: user.verified,
+      message: "Login Success!",
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.mesage });
   }
 };
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,60 @@
+import { Form, Formik } from "formik";
+import { Link } from "react-router-dom";
+import LoginInput from "../../components/inputs/logininput";
+
+import "./style.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <div className="login_wrap">
+          <div className="login_1">
+            <img src="../../icons/facebook.svg" alt="" />
+            <span>
+              Facebook helps you connect and share with the people in your life.
+            </span>
+          </div>
+          <div className="login_2">
+            <div className="login_2_wrap">
+              <Formik
+                initialValues={{
+                  email: "",
+                  password: "",
+                }}
+              >
+                {(formik) => (
+                  <Form>
+                    <LoginInput
+                      type="text"
+                      name="email"
+                      placeholder="Email address or Phone number"
+                    />
+                    <LoginInput
+                      type="text"
+                      name="password"
+                      placeholder="Password"
+                    />
+                    <button type="submit" className="blue_btn">
+                      Log In
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+              <Link to="/forgot" className="forgot_password">
+                Forgotten password?
+              </Link>
+              <div className="sign_splitter"></div>
+              <button className="blue_btn open_signup">Create Account</button>
+            </div>
+            <Link to="/" className="sign_extra">
+              <b>Create a Page </b>
+              for a celebrity. brand or business.
+            </Link>
+          </div>
+        </div>
+        <div className="register"></div>
+      </div>
+    </div>
+  );
+}
